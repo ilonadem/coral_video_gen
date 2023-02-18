b70e0d6517a87ecfc9d4f6331bb1e102d7c47a38
ilonadem <alenkademler@gmail.com>
2023-02-17 20:00:22 -0500

    google sdk installation script

---
+++ install_sdk.sh
@@ -0,0 +1,9 @@
+# download Linux 64-bit archive file
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-418.0.0-linux-x86_64.tar.gz
+
+# unzip file
+tar -xf google-cloud-cli-418.0.0-linux-x86.tar.gz
+
+# add google cloud CLI to path
+bash ./google-cloud-sdk/install.sh
+
